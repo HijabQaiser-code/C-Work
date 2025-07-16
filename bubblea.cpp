351e7260b432201dfe17fa0d143ade5104b582f9
Hijab <qhijab67@gmail.com>
2025-07-16 20:36:55 +0500

    Add files via upload
    # Bubble Sort Integers Program
    This C++ program accepts five integers from the user, sorts them in ascending order using the bubble sort algorithm, and then displays the sorted list. The program repeatedly compares adjacent elements and swaps them if they are in the wrong order, continuing until the entire list is sorted.
    
    ## Features
    - Accepts five integers as input from the user.
    - Stores the integers in an array.
    - Uses the bubble sort algorithm to sort the array in ascending order.
    - Displays the sorted integers after sorting is complete.

---
+++ bubblea.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Setting variables and arrays
+    int arr[5];
+    bool sort = false;
+    int temp = 0;
+
+    // Taking inputs for sorting nd storing them in an array
+    for (int c = 0; c < 5; c++)
+    {
+        cout << "Enter the " << c + 1 << " integer" << endl;
+        cin >> arr[c];
+    }
+
+    // Process
+    for (int a = 0; a < 5; a++)
+    {
+        sort = false;
+        for (int b = 0; b < 4 - a; b++)
+        { // Inner loop gose up to 4 - a to avoid out-of-bounds
+            if (arr[b] > arr[b + 1])
+            {
+                // Swap if the current element is greater than the next
+                temp = arr[b];
+                arr[b] = arr[b + 1];
+                arr[b + 1] = temp;
+                sort = true;
+            }
+        }
+
+        // If no swapping occurred, break out of the loop
+        if (!sort)
+        {
+            break;
+        }
+    }
+
+    // Output
+    cout << "Showing the integers you entered in asscending order " << endl;
+    for (int c = 0; c < 5; c++)
+    {
+        cout << arr[c] << endl;
+    }
+
+    return 0;
+}
